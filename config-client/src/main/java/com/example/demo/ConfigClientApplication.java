bf19cbd3fc7d142cea4fbb83fe1061de4907f382
wangtianjing <wangtianjing@bbdservice.com>
2017-09-13 15:15:56 +0800

    spring cloud 学习

--- config-client/src/main/java/com/example/demo/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigClientApplication {
 
-    @Value("${foo}")
+    //    @Value("${foo}")
     String foo;
 
     public static void main(String[] args) {
