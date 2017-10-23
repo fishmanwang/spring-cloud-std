fb325c199fb92ea54c4fba10d703cf7041369d71
fishmanwang <379590010@qq.com>
2017-10-23 19:11:10 +0800

    springcloud study

--- service-feign/src/main/java/com/example/demo/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/example/demo/ServiceFeignApplication.java
@@ -2,13 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 @EnableHystrixDashboard
 public class ServiceFeignApplication {
 
