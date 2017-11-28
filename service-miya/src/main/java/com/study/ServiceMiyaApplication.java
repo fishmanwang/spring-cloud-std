06f5f652ae2f2b5754b634ebc27734f7e87bf1a6
wangtianjing <wangtianjing@bbdservice.com>
2017-11-28 18:07:54 +0800

    spring cloud 学习

--- service-miya/src/main/java/com/study/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/study/ServiceMiyaApplication.java
@@ -5,40 +5,40 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceMiyaApplication {
 
-	public static final Logger logger = LoggerFactory.getLogger(ServiceMiyaApplication.class);
+    public static final Logger logger = LoggerFactory.getLogger(ServiceMiyaApplication.class);
+    @Autowired
+    private RestTemplate       restTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceMiyaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMiyaApplication.class, args);
+    }
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+    @RequestMapping("/hi")
+    public String home() {
+        logger.info("hi is being called");
+        return "Hi I'm miya";
+    }
 
-
-	@RequestMapping("/hi")
-	public String home() {
-		logger.info("hi is being called");
-		return "Hi I'm miya";
-	}
-
-	@RequestMapping("/miya")
-	public String info() {
-		logger.info("hi is being called");
-		return restTemplate.getForObject("http://localhost:8988/info", String.class);
-	}
+    @RequestMapping("/miya")
+    public String info() {
+        logger.info("hi is being called");
+        return restTemplate.getForObject("http://localhost:8988/info", String.class);
+    }
 
 }
